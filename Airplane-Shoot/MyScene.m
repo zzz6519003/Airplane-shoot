e1cef34beb53944b2c0f56482212bfcd009d31a7
zzz6519003 <zhengzhongzhao@gmail.com>
2013-10-28 23:57:58 +0800

    solve the goddamn bug

--- Airplane-Shoot/MyScene.m
+++ Airplane-Shoot/MyScene.m
@@ -1,9 +1,9 @@
 //
 //  MyScene.m
-//  Airplane-Shoot
+//  AirPlanesGame
 //
-//  Created by Snowmanzzz on 13-10-26.
-//  Copyright (c) 2013年 zzz. All rights reserved.
+//  Created by Jorge Costa on 8/26/13.
+//  Copyright (c) 2013 Jorge Costa. All rights reserved.
 //
 
 #import "MyScene.h"
@@ -12,6 +12,11 @@
 
 -(id)initWithSize:(CGSize)size {
     if (self = [super initWithSize:size]) {
+        /* Setup your scene here */
+        
+        self.physicsWorld.gravity = CGVectorMake(0, 0);
+        self.physicsWorld.contactDelegate = self;
+        
         //init several sizes used in all scene
         screenRect = [[UIScreen mainScreen] bounds];
         screenHeight = screenRect.size.height;
@@ -30,27 +35,32 @@
         background.position = CGPointMake(CGRectGetMidX(self.frame),CGRectGetMidY(self.frame));
         [self addChild:background];
         
+        //adding airplane shadow
         _planeShadow = [SKSpriteNode spriteNodeWithImageNamed:@"PLANE 8 SHADOW.png"];
         _planeShadow.scale = 0.6;
         _planeShadow.zPosition = 1;
         _planeShadow.position = CGPointMake(screenWidth/2+15, 0+_planeShadow.size.height/2);
         [self addChild:_planeShadow];
         
+        //adding propeller animation
         _propeller = [SKSpriteNode spriteNodeWithImageNamed:@"PLANE PROPELLER 1.png"];
         _propeller.scale = 0.2;
         _propeller.position = CGPointMake(screenWidth/2, _plane.size.height+10);
+        
         SKTexture *propeller1 = [SKTexture textureWithImageNamed:@"PLANE PROPELLER 1.png"];
         SKTexture *propeller2 = [SKTexture textureWithImageNamed:@"PLANE PROPELLER 2.png"];
+        
         SKAction *spin = [SKAction animateWithTextures:@[propeller1,propeller2] timePerFrame:0.1];
         SKAction *spinForever = [SKAction repeatActionForever:spin];
         [_propeller runAction:spinForever];
+        
         [self addChild:_propeller];
         
         
         //CoreMotion
         self.motionManager = [[CMMotionManager alloc] init];
         self.motionManager.accelerometerUpdateInterval = .2;
-        //        self.motionManager.gyroUpdateInterval = .2;
+        //self.motionManager.gyroUpdateInterval = .2;
         
         [self.motionManager startAccelerometerUpdatesToQueue:[NSOperationQueue currentQueue]
                                                  withHandler:^(CMAccelerometerData  *accelerometerData, NSError *error) {
@@ -72,9 +82,9 @@
         SKAction *callEnemies = [SKAction runBlock:^{
             [self EnemiesAndClouds];
         }];
+        
         SKAction *updateEnimies = [SKAction sequence:@[wait,callEnemies]];
         [self runAction:[SKAction repeatActionForever:updateEnimies]];
-        
         
         //load explosions
         SKTextureAtlas *explosionAtlas = [SKTextureAtlas atlasNamed:@"EXPLOSION"];
@@ -84,6 +94,7 @@
             SKTexture *texture = [explosionAtlas textureNamed:name];
             [_explosionTextures addObject:texture];
         }
+        
         //load clouds
         SKTextureAtlas *cloudsAtlas = [SKTextureAtlas atlasNamed:@"Clouds"];
         NSArray *textureNamesClouds = [cloudsAtlas textureNames];
@@ -100,43 +111,52 @@
 -(void)EnemiesAndClouds{
     //not always come
     int GoOrNot = [self getRandomNumberBetween:0 to:1];
+    
     if(GoOrNot == 1){
+        
         SKSpriteNode *enemy;
+        
         int randomEnemy = [self getRandomNumberBetween:0 to:1];
         if(randomEnemy == 0)
             enemy = [SKSpriteNode spriteNodeWithImageNamed:@"PLANE 1 N.png"];
         else
             enemy = [SKSpriteNode spriteNodeWithImageNamed:@"PLANE 2 N.png"];
+        
+        
         enemy.scale = 0.6;
+        
         enemy.position = CGPointMake(screenRect.size.width/2, screenRect.size.height/2);
         enemy.zPosition = 1;
+        
+        
         CGMutablePathRef cgpath = CGPathCreateMutable();
+        
         //random values
         float xStart = [self getRandomNumberBetween:0+enemy.size.width to:screenRect.size.width-enemy.size.width ];
         float xEnd = [self getRandomNumberBetween:0+enemy.size.width to:screenRect.size.width-enemy.size.width ];
+        
         //ControlPoint1
         float cp1X = [self getRandomNumberBetween:0+enemy.size.width to:screenRect.size.width-enemy.size.width ];
         float cp1Y = [self getRandomNumberBetween:0+enemy.size.width to:screenRect.size.width-enemy.size.height ];
+        
         //ControlPoint2
         float cp2X = [self getRandomNumberBetween:0+enemy.size.width to:screenRect.size.width-enemy.size.width ];
         float cp2Y = [self getRandomNumberBetween:0 to:cp1Y];
+        
         CGPoint s = CGPointMake(xStart, 1024.0);
         CGPoint e = CGPointMake(xEnd, -100.0);
         CGPoint cp1 = CGPointMake(cp1X, cp1Y);
         CGPoint cp2 = CGPointMake(cp2X, cp2Y);
         CGPathMoveToPoint(cgpath,NULL, s.x, s.y);
         CGPathAddCurveToPoint(cgpath, NULL, cp1.x, cp1.y, cp2.x, cp2.y, e.x, e.y);
-//        SKAction *planeDestroy = [SKAction followPath:cgpath asOffset:NO orientToPath:YES duration:5];
+        
+        SKAction *planeDestroy = [SKAction followPath:cgpath asOffset:NO orientToPath:YES duration:5];
         [self addChild:enemy];
+        
         SKAction *remove = [SKAction removeFromParent];
-//        [enemy runAction:[SKAction sequence:@[planeDestroy,remove]]];
-        [enemy runAction:[SKAction sequence:@[remove]]];
+        [enemy runAction:[SKAction sequence:@[planeDestroy,remove]]];
+        
         CGPathRelease(cgpath);
-        
-        
-        self.physicsWorld.gravity = CGVectorMake(0, 0);
-        self.physicsWorld.contactDelegate = self;
-        
         
         enemy.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:enemy.size];
         enemy.physicsBody.dynamic = YES;
@@ -144,24 +164,29 @@
         enemy.physicsBody.contactTestBitMask = bulletCategory;
         enemy.physicsBody.collisionBitMask = 0;
         
-        
         //random Clouds
         int randomClouds = [self getRandomNumberBetween:0 to:1];
         if(randomClouds == 1){
+            
             int whichCloud = [self getRandomNumberBetween:0 to:3];
             SKSpriteNode *cloud = [SKSpriteNode spriteNodeWithTexture:[_cloudsTextures objectAtIndex:whichCloud]];
             int randomYAxix = [self getRandomNumberBetween:0 to:screenRect.size.height];
             cloud.position = CGPointMake(screenRect.size.height+cloud.size.height/2, randomYAxix);
-            cloud.zPosition = 110;
+            cloud.zPosition = 1;
             int randomTimeCloud = [self getRandomNumberBetween:9 to:19];
+            
             SKAction *move =[SKAction moveTo:CGPointMake(0-cloud.size.height, randomYAxix) duration:randomTimeCloud];
             SKAction *remove = [SKAction removeFromParent];
             [cloud runAction:[SKAction sequence:@[move,remove]]];
             [self addChild:cloud];
         }
+        
     }
+    
 }
+
 -(int)getRandomNumberBetween:(int)from to:(int)to {
+    
     return (int)from + arc4random() % (to-from+1);
 }
 
@@ -178,48 +203,6 @@
     {
         currentMaxAccelY = acceleration.y;
     }
-    
-    
-}
-
-
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch begins */
-    
-    //    for (UITouch *touch in touches) {
-    //        CGPoint location = [touch locationInNode:self];
-    //
-    //        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-    //
-    //        sprite.position = location;
-    //
-    //        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-    //
-    //        [sprite runAction:[SKAction repeatActionForever:action]];
-    //
-    //        [self addChild:sprite];
-    //    }
-    
-    /* Called when a touch begins */
-    CGPoint location = [_plane position];
-    SKSpriteNode *bullet = [SKSpriteNode spriteNodeWithImageNamed:@"B 2.png"];
-    bullet.position = CGPointMake(location.x,location.y+_plane.size.height/2);
-    //bullet.position = location;
-    bullet.zPosition = 1;
-    bullet.scale = 0.8;
-    SKAction *action = [SKAction moveToY:self.frame.size.height+bullet.size.height duration:2];
-    SKAction *remove = [SKAction removeFromParent];
-    [bullet runAction:[SKAction sequence:@[action,remove]]];
-    
-    
-    bullet.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:bullet.size];
-    bullet.physicsBody.dynamic = NO;
-    bullet.physicsBody.categoryBitMask = bulletCategory;
-    bullet.physicsBody.contactTestBitMask = enemyCategory;
-    bullet.physicsBody.collisionBitMask = 0;
-    
-    [self addChild:bullet];
-    
     
     
 }
@@ -272,14 +255,40 @@
     _plane.position = CGPointMake(newX, newY);
     _planeShadow.position = CGPointMake(newXshadow, newYshadow);
     _propeller.position = CGPointMake(newXpropeller, newYpropeller);
-    //_smokeTrail.position = CGPointMake(newX,newY-(_plane.size.height/2));
     _smokeTrail.position = CGPointMake(newX,newY-(_plane.size.height/2));
+}
+
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    /* Called when a touch begins */
     
+    CGPoint location = [_plane position];
+    SKSpriteNode *bullet = [SKSpriteNode spriteNodeWithImageNamed:@"B 2.png"];
+    
+    bullet.position = CGPointMake(location.x,location.y+_plane.size.height/2);
+    //bullet.position = location;
+    bullet.zPosition = 1;
+    bullet.scale = 0.8;
+    
+    bullet.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:bullet.size];
+    bullet.physicsBody.dynamic = NO;
+    bullet.physicsBody.categoryBitMask = bulletCategory;
+    bullet.physicsBody.contactTestBitMask = enemyCategory;
+    bullet.physicsBody.collisionBitMask = 0;
+    
+    SKAction *action = [SKAction moveToY:self.frame.size.height+bullet.size.height duration:2];
+    SKAction *remove = [SKAction removeFromParent];
+    
+    [bullet runAction:[SKAction sequence:@[action,remove]]];
+    
+    [self addChild:bullet];
 }
 
 -(void)didBeginContact:(SKPhysicsContact *)contact{
+    
     SKPhysicsBody *firstBody;
     SKPhysicsBody *secondBody;
+    
     if (contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask)
     {
         firstBody = contact.bodyA;
@@ -290,26 +299,29 @@
         firstBody = contact.bodyB;
         secondBody = contact.bodyA;
     }
+    
     if ((firstBody.categoryBitMask & bulletCategory) != 0)
     {
+        
         SKNode *projectile = (contact.bodyA.categoryBitMask & bulletCategory) ? contact.bodyA.node : contact.bodyB.node;
         SKNode *enemy = (contact.bodyA.categoryBitMask & bulletCategory) ? contact.bodyB.node : contact.bodyA.node;
         [projectile runAction:[SKAction removeFromParent]];
         [enemy runAction:[SKAction removeFromParent]];
-        
         
         //add explosion
         SKSpriteNode *explosion = [SKSpriteNode spriteNodeWithTexture:[_explosionTextures objectAtIndex:0]];
         explosion.zPosition = 1;
         explosion.scale = 0.6;
         explosion.position = contact.bodyA.node.position;
+        
         [self addChild:explosion];
+        
         SKAction *explosionAction = [SKAction animateWithTextures:_explosionTextures timePerFrame:0.07];
         SKAction *remove = [SKAction removeFromParent];
         [explosion runAction:[SKAction sequence:@[explosionAction,remove]]];
         
-        
     }
+    
 }
 
 
